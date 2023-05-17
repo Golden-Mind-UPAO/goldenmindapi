64ed8b1922c1ba4eef3529e06ba6e313a5ed5f15
HalvertCode <hguerrerop1@upao.edu.pe>
2023-05-16 22:32:36 -0500

    US15: Configuracion de proyecto de Spring Boot

---
+++ goldenmindapi/src/main/java/com/goldenmind/goldenmindapi/GoldenmindapiApplication.java
@@ -0,0 +1,13 @@
+package com.goldenmind.goldenmindapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenmindapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenmindapiApplication.class, args);
+	}
+
+}
---
+++ goldenmindapi/src/test/java/com/goldenmind/goldenmindapi/GoldenmindapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldenmind.goldenmindapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenmindapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
