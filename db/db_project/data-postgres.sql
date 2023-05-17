734d4bc68922d842d26d536ec7f9670ab28b87a3
HalvertCode <hguerrerop1@upao.edu.pe>
2023-05-16 23:19:12 -0500

    US16: Configuracion de datos iniciales

---
+++ db/db_project/data-postgres.sql
@@ -0,0 +1,13 @@
+-- Database: GoldenMind_db
+
+-- DROP DATABASE IF EXISTS "GoldenMind_db";
+
+CREATE DATABASE "GoldenMind_db"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Peru.1252'
+    LC_CTYPE = 'Spanish_Peru.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
